93af038a50540c83ea11d3e200ec5eab55a826ef
André Norman Lang <andrenormanlang@gmail.com>
2024-05-16 17:56:52 +0200

    todo CRUD htmx-go-templ

--- static/script.js
+++ static/script.js
@@ -1,11 +1,15 @@
 function toggleEditForm(taskID) {
     const editForm = document.getElementById(`edit-task-form-${taskID}`);
     const taskText = document.getElementById(`task-text-${taskID}`);
+    const buttons = document.getElementById(`task-buttons-${taskID}`);
+    
     if (editForm.style.display === 'none') {
         editForm.style.display = 'block';
         taskText.style.display = 'none';
+        buttons.style.display = 'none';  // Hide the Edit and Delete buttons
     } else {
         editForm.style.display = 'none';
         taskText.style.display = 'block';
+        buttons.style.display = 'flex';  // Show the Edit and Delete buttons
     }
 }
